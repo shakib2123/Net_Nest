a2d670d2da3545aee4ca4ad16cc300b9ae902446
shakib2123 <mshakibalhasan21@gmail.com>
2024-02-03 02:05:42 +0600

    sidebar auto collapse on mobile method implimented

--- app/(browse)/_components/container.tsx
+++ app/(browse)/_components/container.tsx
@@ -2,12 +2,25 @@
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
 
+import { useEffect } from "react";
+import { useMediaQuery } from "usehooks-ts";
+
 interface ContainerProps {
   children: React.ReactNode;
 }
 
 export const Container = ({ children }: ContainerProps) => {
-  const { collapsed } = useSidebar((state) => state);
+  const matches = useMediaQuery("(max-width:1024px)");
+  const { collapsed, onCollapse, onExpend } = useSidebar((state) => state);
+
+  useEffect(() => {
+    if (matches) {
+      onCollapse();
+    } else {
+      onExpend();
+    }
+  }, [matches, onCollapse, onExpend]);
+
   return (
     <div
       className={cn("flex-1", collapsed ? "ml-[70px]" : "ml-[70px] lg:ml-60")}
