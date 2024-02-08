46a5f1adf0a639930bf5400e47f55ebb17a9464f
shakib2123 <mshakibalhasan21@gmail.com>
2024-02-08 23:56:13 +0600

    creator dashboard container created

--- app/(dashboard)/u/[username]/_components/container.tsx
+++ app/(dashboard)/u/[username]/_components/container.tsx
@@ -10,5 +10,24 @@ interface ContainerProps {
 }
 
 export const Container = ({ children }: ContainerProps) => {
-  return <div>{children}</div>;
+  const { collapsed, onCollapse, onExpand } = useCreatorSidebar(
+    (state) => state
+  );
+  const matches = useMediaQuery(`(max-width: 1024px)`);
+
+  useEffect(() => {
+    if (matches) {
+      onCollapse();
+    } else {
+      onExpand();
+    }
+  }, [matches, onCollapse, onExpand]);
+
+  return (
+    <div
+      className={cn("flex-1", collapsed ? "ml-[70px]" : "ml-[70px] lg:ml-60")}
+    >
+      {children}
+    </div>
+  );
 };
