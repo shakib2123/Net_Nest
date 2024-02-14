c3da948343695e03c3fcf7fddd3affdddae2c2a0
shakib2123 <mshakibalhasan21@gmail.com>
2024-02-14 22:08:43 +0600

    chat toggle created

---
+++ components/stream-player/chat.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useEffect, useMemo, useState } from "react";
+import { ConnectionState } from "livekit-client";
+import {
+  useChat,
+  useConnectionState,
+  useRemoteParticipant,
+} from "@livekit/components-react";
+import { useMediaQuery } from "usehooks-ts";
+
+import { useChatSidebar } from "@/store/use-chat-sidebar";
+import { ChatHeader } from "./chat-header";
+
+interface ChatProps {
+  viewerName: string;
+  hostName: string;
+  hostIdentity: string;
+  isFollowing: boolean;
+  isChatEnabled: boolean;
+  isChatDelayed: boolean;
+  isChatFollowersOnly: boolean;
+}
+
+export const Chat = ({
+  viewerName,
+  hostName,
+  hostIdentity,
+  isFollowing,
+  isChatEnabled,
+  isChatDelayed,
+  isChatFollowersOnly,
+}: ChatProps) => {
+  const matches = useMediaQuery("(max-width: 1024px)");
+  const { variant, onExpand } = useChatSidebar((state) => state);
+  const connectionState = useConnectionState();
+  const participant = useRemoteParticipant(hostIdentity);
+
+  const isOnline = participant && connectionState === ConnectionState.Connected;
+
+  const isHidden = !isChatEnabled || !isOnline;
+
+  const [value, setValue] = useState("");
+  const { chatMessages: messages, send } = useChat();
+
+  useEffect(() => {
+    if (matches) {
+      onExpand();
+    }
+  }, [matches, onExpand]);
+
+  const reversedMessage = useMemo(() => {
+    return messages.sort((a, b) => b.timestamp - a.timestamp);
+  }, [messages]);
+
+  const onSubmit = () => {
+    if (!send) return;
+
+    send(value);
+    setValue("");
+  };
+
+  const onChange = (value: string) => {
+    setValue(value);
+  };
+
+  return (
+    <div className="flex flex-col bg-background border-l border-b pt-0 h-[calc(100vh-80px)]">
+      <ChatHeader/>
+    </div>
+  );
+};
--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -3,9 +3,14 @@
 import User from "@/utils/models/user";
 import Stream from "@/utils/models/Stream";
 
+import { useChatSidebar } from "@/store/use-chat-sidebar";
+import { cn } from "@/lib/utils";
+
 import { LiveKitRoom } from "@livekit/components-react";
 import { useViewerToken } from "@/hooks/use-viewer-token";
 import { Video } from "./video";
+import { Chat } from "./chat";
+import { ChatToggle } from "./chat-toggle";
 
 interface StreamPlayerProps {
   user: User;
@@ -20,19 +25,42 @@ export const StreamPlayer = ({
 }: StreamPlayerProps) => {
   const { token, name, identity } = useViewerToken(user?._id);
 
+  const { collapsed } = useChatSidebar((state) => state);
+
   if (!token || !name || !identity) {
     return <div>Cannot watch the stream</div>;
   }
 
   return (
     <>
+      {collapsed && (
+        <div className="hidden lg:block fixed top-[100px] right-2 z-50">
+          <ChatToggle />
+        </div>
+      )}
       <LiveKitRoom
         token={token}
         serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
-        className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+        className={cn(
+          "grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full",
+          collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2"
+        )}
       >
         <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
           <Video hostName={user?.username} hostIdentity={user?._id} />
+        </div>
+
+        {/* chat bar */}
+        <div className={cn("col-span-1", collapsed && "hidden")}>
+          <Chat
+            viewerName={name}
+            hostName={user.username}
+            hostIdentity={user._id}
+            isFollowing={isFollowing}
+            isChatEnabled={stream.isChatEnabled}
+            isChatDelayed={stream.isChatDelayed}
+            isChatFollowersOnly={stream.isChatFollowersOnly}
+          />
         </div>
       </LiveKitRoom>
     </>
