21e2fcf86fc07d33a6ad61e8c5eeae2b7f367572
shakib2123 <mshakibalhasan21@gmail.com>
2024-02-24 22:19:04 +0600

    error handling improved

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,10 +1,9 @@
 import { NextResponse } from "next/server";
-import connectDB from "@/utils/mongoose/db";
+
 import User from "../../../utils/models/User";
 
 export const GET = async (request: any): Promise<any> => {
   try {
-    await connectDB();
     const posts = await User.find();
     return NextResponse.json(posts, { status: 200 });
   } catch (error) {
